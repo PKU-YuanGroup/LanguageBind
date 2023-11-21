f6b855fb09d7463d54306080da5071bc8693cab0
lb203 <62638829+LinB203@users.noreply.github.com>
2023-11-21 22:09:53 +0800

    Update eval.sh

--- scripts/video_language/eval.sh
+++ scripts/video_language/eval.sh
@@ -1,12 +1,12 @@
 
 CACHE_DIR="path/to/pretrained/weight"
 RESUME="video_language.pt"
-TRAIN_DATA="path/to/data"
+ANNOTATION="path/to/data"
 # this script is for 640 total batch_size (n(16) GPUs * batch_size(10) * accum_freq(4))
 cd /path/to/LanguageBind
 TORCH_DISTRIBUTED_DEBUG=DETAIL HF_DATASETS_OFFLINE=1 TRANSFORMERS_OFFLINE=1 torchrun --nnodes=$HOST_NUM --node_rank=$INDEX --nproc_per_node $HOST_GPU_NUM --master_addr $CHIEF_IP \
     -m main  \
-    --train-data ${TRAIN_DATA} \
+    --train-data ${ANNOTATION} \
     --train-num-samples 3020000 \
     --clip-type "vl" \
     --lock-text --lock-image --text-type "polish_mplug" \
