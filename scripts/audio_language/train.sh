bb1b700bbbb9f43599c74869a42cf065c6fb2b83
lb203 <62638829+LinB203@users.noreply.github.com>
2023-11-21 22:09:01 +0800

    Update train.sh

--- scripts/audio_language/train.sh
+++ scripts/audio_language/train.sh
@@ -1,11 +1,11 @@
 
 CACHE_DIR="path/to/pretrained/weight"
-TRAIN_DATA="path/to/data"
+ANNOTATION="path/to/data"
 # this script is for 1024 total batch_size (n(16) GPUs * batch_size(16) * accum_freq(4))
 cd /path/to/LanguageBind
 TORCH_DISTRIBUTED_DEBUG=DETAIL HF_DATASETS_OFFLINE=1 TRANSFORMERS_OFFLINE=1 torchrun --nnodes=2 --nproc_per_node 8 \
     -m main  \
-    --train-data ${TRAIN_DATA} \
+    --train-data ${ANNOTATION} \
     --train-num-samples 4800000 \
     --clip-type "al" --num_mel_bins 126 --target_length 1036 --audio_sample_rate 16000 --audio_mean -4.2677393 --audio_std 4.5689974 \
     --lock-text --lock-image --text-type "polish_mplug" \
